7d9c4bae1efe9759582129ffabe0b156ae76bee1
khanr97 <tafirnahk@gmail.com>
2022-10-13 13:55:13 -0400

    Fizz Buzz answered

---
+++ javascript-reps.js
@@ -0,0 +1,9 @@
+//Part 2====Javascript Reps=======
+for(i=0;i<=20;i++) {
+    console.log(i)
+}
+//Write a for loop that will log only the even numbers
+// in 0 through 200.
+for(i=0;i<=200;i+=2) {
+    console.log(i)
+}
